866bc6c10fdbd459b5e3077cdee4d3806c398019
sage-etcher <sage.message@email.com>
2024-12-07 16:43:36 -0500

    sei.h: use popcountll builtlin when working with longlongs

--- src/sei.h
+++ src/sei.h
@@ -8,10 +8,13 @@
 
 #define clz(x) __builtin_clz(x)
 #define popcount(x) __builtin_popcount(x)
+#define popcountll(x) __builtin_popcountll(x)
 #define PRECISION(x) popcount (x)
+#define PRECISIONLL(x) popcountll (x)
 
 #define SEI_LSHFT_U(max, a) (a >= PRECISION (max))
-#define SEI_LSHFT_SIZE(a) SEI_LSHFT_U (SIZE_MAX, a)
+#define SEI_LSHFT_ULL(max, a) (a >= PRECISIONLL (max))
+#define SEI_LSHFT_SIZE(a) SEI_LSHFT_ULL (SIZE_MAX, a)
 
 #define SEI_SUB_U(a, b) ((a) < (b))
 #define SEI_SUB_SIZE(a, b) SEI_SUB_U (a, b)
